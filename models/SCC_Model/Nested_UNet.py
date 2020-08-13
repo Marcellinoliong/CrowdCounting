910107d1a6d4e0518a2da33281bd32d836a95413
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-08-13 13:18:51 +0700

    Update Nested_UNet.py

--- models/SCC_Model/Nested_UNet.py
+++ models/SCC_Model/Nested_UNet.py
@@ -9,9 +9,9 @@ class conv_block_nested(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
         super(conv_block_nested, self).__init__()
         self.activation = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=3, padding=1, bias=True)
+        self.conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=1, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(mid_ch)
-        self.conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=3, padding=1, bias=True)
+        self.conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=1, padding=1, bias=True)
         self.bn2 = nn.BatchNorm2d(out_ch)
 
         #self.seq = nn.Sequential(nn.Conv2d(in_ch, mid_ch, 3, same_padding=True, NL='relu'),
@@ -32,7 +32,7 @@ class conv_block_nested(nn.Module):
 
 class Nested_UNet(nn.Module):
 
-    def __init__(self, in_ch=1, out_ch=1,  pretrained=True):
+    def __init__(self, in_ch=1, out_ch=64,  pretrained=True):
         super(Nested_UNet, self).__init__()
 
         n1 = 64
