f5735aac2d9760c0a3f70bc5cf42ca3080100772
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-08-14 02:13:09 +0700

    Update Nested_UNet2.py

--- models/UNet/Nested_UNet2.py
+++ models/UNet/Nested_UNet2.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .layers import unetConv2, unetUp_origin
-from init_weights import init_weights
+from .init_weights import init_weights
 import numpy as np
 class Nested_UNet2(nn.Module):
 
