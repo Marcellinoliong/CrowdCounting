f0d759e2049046fee309f9bd82f6a85d0c9675ce
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-08-14 03:11:06 +0700

    Update Nested_UNet_Efficient.py

--- models/UNet/Nested_UNet_Efficient.py
+++ models/UNet/Nested_UNet_Efficient.py
@@ -45,7 +45,7 @@ class conv_block_nested(nn.Module):
 class Nested_UNet_Efficient(nn.Module):
 
     def __init__(self, in_ch=3, out_ch=1,  pretrained=True):
-        super(Nested_UNet, self).__init__()
+        super(Nested_UNet_Efficient, self).__init__()
 
         n1 = 64
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
