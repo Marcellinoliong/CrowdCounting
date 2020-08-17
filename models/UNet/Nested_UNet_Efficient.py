5916211f23ba40252e11dd764a29f2f8558c37b6
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-08-17 23:32:58 +0700

    Update Nested_UNet_Efficient.py

--- models/UNet/Nested_UNet_Efficient.py
+++ models/UNet/Nested_UNet_Efficient.py
@@ -84,7 +84,7 @@ class Nested_UNet_Efficient(nn.Module):
             self.final = nn.Sequential(nn.Conv2d(filters[0], out_ch, kernel_size=1), self.activation)
 
         self.res = EfficientNet.from_pretrained('efficientnet-b7')
-        self.res.in_channels = 64
+        self.res.in_channels = n1
         self.frontend = nn.Sequential(
            self.res._conv_stem, self.res._bn0, self.res._swish
         )
