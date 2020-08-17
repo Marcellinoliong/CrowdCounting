2d2f9e04391b2c41929c77287cb227d0b8fa0036
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-08-17 23:57:21 +0700

    Update loading_data.py

--- datasets/SHHA/loading_data.py
+++ datasets/SHHA/loading_data.py
@@ -28,15 +28,15 @@ def random_crop(img,den,dst_size):
     # print img.shape
     # print den.shape
 
-    x1 = random.randint(0, ts_wd - dst_size[1])/cfg_data.LABEL_FACTOR*cfg_data.LABEL_FACTOR
-    y1 = random.randint(0, ts_hd - dst_size[0])/cfg_data.LABEL_FACTOR*cfg_data.LABEL_FACTOR
+    x1 = random.randint(0, ts_wd - dst_size[1])//cfg_data.LABEL_FACTOR*cfg_data.LABEL_FACTOR
+    y1 = random.randint(0, ts_hd - dst_size[0])//cfg_data.LABEL_FACTOR*cfg_data.LABEL_FACTOR
     x2 = x1 + dst_size[1]
     y2 = y1 + dst_size[0]
 
-    label_x1 = x1/cfg_data.LABEL_FACTOR
-    label_y1 = y1/cfg_data.LABEL_FACTOR
-    label_x2 = x2/cfg_data.LABEL_FACTOR
-    label_y2 = y2/cfg_data.LABEL_FACTOR
+    label_x1 = x1//cfg_data.LABEL_FACTOR
+    label_y1 = y1//cfg_data.LABEL_FACTOR
+    label_x2 = x2//cfg_data.LABEL_FACTOR
+    label_y2 = y2//cfg_data.LABEL_FACTOR
 
     return img[:,y1:y2,x1:x2], den[label_y1:label_y2,label_x1:label_x2]
 
