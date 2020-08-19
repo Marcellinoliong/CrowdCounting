d0326887dad035505ce11af22cdfcc3f46e3a443
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-08-19 13:50:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -99,7 +99,7 @@ def test(file_list, model_path):
             gt = 0
             imgp = img
             denp = den
-            while gt < 20 :
+            while gt < 25 :
                 x1 = random.randint(0, ts_wd - dst_size[1])
                 y1 = random.randint(0, ts_hd - dst_size[0])
                 x2 = x1 + dst_size[1]
@@ -109,7 +109,6 @@ def test(file_list, model_path):
                 denp = den[y1:y2,x1:x2]
 
                 gt = np.sum(denp)
-                print( filename +' gt:'+ str(int(gt)))
 
             with torch.no_grad():
                 imgp = Variable(imgp[None,:,:,:]).cuda()
