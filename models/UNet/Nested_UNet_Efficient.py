6c2b760a2c1be58036ce9922260c11f6de95057a
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-10-29 00:46:05 +0700

    new

--- models/UNet/Nested_UNet_Efficient.py
+++ models/UNet/Nested_UNet_Efficient.py
@@ -111,27 +111,35 @@ class Nested_UNet_Efficient(nn.Module):
         x1_1 = self.conv1_1(torch.cat([x1_0, F.interpolate(x2_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, F.interpolate(x1_1, scale_factor=2, mode='bilinear', align_corners=True)], 1))
 
-        x3_0 = self.conv3_0(self.pool(x2_0))
+        #x3_0 = self.conv3_0(self.pool(x2_0))
+        x3_0 = self.res.endpoints['reduction_4'].shape
         x2_1 = self.conv2_1(torch.cat([x2_0, F.interpolate(x3_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, F.interpolate(x2_1, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, F.interpolate(x1_2, scale_factor=2, mode='bilinear', align_corners=True)], 1))
 
-        x4_0 = self.conv4_0(self.pool(x3_0))
+        #x4_0 = self.conv4_0(self.pool(x3_0))
+        x4_0 = self.res.endpoints['reduction_6'].shape
         x3_1 = self.conv3_1(torch.cat([x3_0, F.interpolate(x4_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, F.interpolate(x3_1, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, F.interpolate(x2_2, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, F.interpolate(x1_3, scale_factor=2, mode='bilinear', align_corners=True)], 1))
 
-        if self.deep_supervision:
-            output1 = self.final1(x0_1)
-            output2 = self.final2(x0_2)
-            output3 = self.final3(x0_3)
-            output4 = self.final4(x0_4)
-            return (output1 + output2 + output3 + output4)/4
+        #if self.deep_supervision:
+        #    output1 = self.final1(x0_1)
+        #    output2 = self.final2(x0_2)
+        #    output3 = self.final3(x0_3)
+        #    output4 = self.final4(x0_4)
+        #    return (output1 + output2 + output3 + output4)/4
 
-        else:
-            output = self.final(x0_4)
-            return output
+        #else:
+        #    output = self.final(x0_4)
+        #    return output
+        output1 = self.final1(x0_1)
+        output2 = self.final2(x0_2)
+        output3 = self.final3(x0_3)
+        output4 = self.final4(x0_4)
+        return output1, output2, output3, output4
+            
             
 if __name__ == '__main__':
     import time
--- trainer.py
+++ trainer.py
@@ -90,8 +90,13 @@ class Trainer():
             gt_map = Variable(gt_map).cuda()
 
             self.optimizer.zero_grad()
-            pred_map = self.net(img, gt_map)
-            loss = self.net.loss
+            pred_map1, pred_map2, pred_map3, pred_map4 = self.net(img, gt_map)
+            loss1 = self.net.build_loss(pred_map1, gt_map)
+            loss2 = self.net.build_loss(pred_map2, gt_map)
+            loss3 = self.net.build_loss(pred_map3, gt_map)
+            loss4 = self.net.build_loss(pred_map4, gt_map)
+            loss = (loss1 + loss2 + loss3 + loss4) / 4
+            #loss = self.net.loss
             loss.backward()
             self.optimizer.step()
 
