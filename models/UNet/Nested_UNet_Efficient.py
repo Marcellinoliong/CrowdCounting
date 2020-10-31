cfff8be1609d38f35f7e4917636dd50cf1fd33d2
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-10-31 13:13:46 +0700

    Update Nested_UNet_Efficient.py

--- models/UNet/Nested_UNet_Efficient.py
+++ models/UNet/Nested_UNet_Efficient.py
@@ -107,9 +107,9 @@ class Nested_UNet_Efficient(nn.Module):
         x0_1 = self.conv0_1(torch.cat([x0_0, F.interpolate(x1_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
 
         #x2_0 = self.conv2_0(self.pool(x1_0))
-        len(endpoints)
+        print(len(endpoints))
         x2_0 = endpoints['reduction_2'].shape
-        x2_0.size()
+        print(x2_0)
         x1_1 = self.conv1_1(torch.cat([x1_0, F.interpolate(x2_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, F.interpolate(x1_1, scale_factor=2, mode='bilinear', align_corners=True)], 1))
 
