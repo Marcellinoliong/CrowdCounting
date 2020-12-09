589725967b6fc659e55b0c8f54b8d2e5c9d7430b
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-12-09 13:39:20 +0700

    Update Nested_UNet_Efficient.py

--- models/UNet/Nested_UNet_Efficient.py
+++ models/UNet/Nested_UNet_Efficient.py
@@ -118,8 +118,8 @@ class Nested_UNet_Efficient(nn.Module):
         #x0_0  = self.frontend(x)
         x0_0  = self.conv0_0(x)
         x1_0 = self.conv1_0(self.pool(x0_0))
-        #print(x0_0.size())
-        #print(x1_0.size())
+        print(x0_0.size())
+        print(x1_0.size())
         #print((F.interpolate(x1_0, scale_factor=2, mode='bilinear', align_corners=True)).size())
         x0_1 = self.conv0_1(torch.cat([x0_0, F.interpolate(x1_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
 
@@ -143,7 +143,7 @@ class Nested_UNet_Efficient(nn.Module):
         #x3_0 = self.conv3_0(self.pool(x2_0))
         x3_0 = x_en
         x3_0 = self.Expand3(x_en)
-        #print(x3_0.size())
+        print(x3_0.size())
         x2_1 = self.conv2_1(torch.cat([x2_0, F.interpolate(x3_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, F.interpolate(x2_1, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, F.interpolate(x1_2, scale_factor=2, mode='bilinear', align_corners=True)], 1))
@@ -161,7 +161,7 @@ class Nested_UNet_Efficient(nn.Module):
         #x4_0 = self.conv4_0(self.pool(x3_0))
         x4_0 = x_en
         x4_0 = self.Expand4(x_en)
-        #print(x4_0.size())
+        print(x4_0.size())
         x3_1 = self.conv3_1(torch.cat([x3_0, F.interpolate(x4_0, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, F.interpolate(x3_1, scale_factor=2, mode='bilinear', align_corners=True)], 1))
         x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, F.interpolate(x2_2, scale_factor=2, mode='bilinear', align_corners=True)], 1))
