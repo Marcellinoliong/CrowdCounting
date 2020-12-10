0909a6e309f572d1ef92bf91711b792bcf339bb7
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-12-10 21:00:29 +0700

    s

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ cfg = __C
 
 #------------------------------TRAIN------------------------
 __C.SEED = 3035 # random seed,  for reporduction
-__C.DATASET = 'UCF50' # dataset selection: GCC, SHHA, SHHB, UCF50, QNRF, WE
+__C.DATASET = 'WE' # dataset selection: GCC, SHHA, SHHB, UCF50, QNRF, WE
 
 if __C.DATASET == 'UCF50':# only for UCF50
 	from datasets.UCF50.setting import cfg_data
