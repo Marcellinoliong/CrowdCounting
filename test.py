5674b2280385e9f0ba5a7a2ae38e9f9ae088af5c
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-12-10 22:08:46 +0700

    Update test.py

--- test.py
+++ test.py
@@ -55,6 +55,100 @@ def test(file_list, model_path):
     gts = []
     preds = []
 
+    for filename in file_list:
+    	print( filename )
+        imgname = dataRoot + '/img/' + filename
+        filename_no_ext = filename.split('.')[0]
+
+        denname = dataRoot + '/den/' + filename_no_ext + '.csv'
+
+        den = pd.read_csv(denname, sep=',',header=None).values
+        den = den.astype(np.float32, copy=False)
+
+        img = Image.open(imgname)
+
+        if img.mode == 'L':
+            img = img.convert('RGB')
+
+
+        img = img_transform(img)
+
+        gt = np.sum(den)
+        with torch.no_grad():
+            img = Variable(img[None,:,:,:]).cuda()
+            pred_map = net.test_forward(img)
+
+        sio.savemat(exp_name+'/pred/'+filename_no_ext+'.mat',{'data':pred_map.squeeze().cpu().numpy()/100.})
+        sio.savemat(exp_name+'/gt/'+filename_no_ext+'.mat',{'data':den})
+
+        pred_map = pred_map.cpu().data.numpy()[0,0,:,:]
+
+
+        pred = np.sum(pred_map)/100.0
+        pred_map = pred_map/np.max(pred_map+1e-20)
+        
+        den = den/np.max(den+1e-20)
+
+        
+        den_frame = plt.gca()
+        plt.imshow(den, 'jet')
+        den_frame.axes.get_yaxis().set_visible(False)
+        den_frame.axes.get_xaxis().set_visible(False)
+        den_frame.spines['top'].set_visible(False) 
+        den_frame.spines['bottom'].set_visible(False) 
+        den_frame.spines['left'].set_visible(False) 
+        den_frame.spines['right'].set_visible(False) 
+        plt.savefig(exp_name+'/'+filename_no_ext+'_gt_'+str(int(gt))+'.png',\
+            bbox_inches='tight',pad_inches=0,dpi=150)
+
+        plt.close()
+        
+        # sio.savemat(exp_name+'/'+filename_no_ext+'_gt_'+str(int(gt))+'.mat',{'data':den})
+
+        pred_frame = plt.gca()
+        plt.imshow(pred_map, 'jet')
+        pred_frame.axes.get_yaxis().set_visible(False)
+        pred_frame.axes.get_xaxis().set_visible(False)
+        pred_frame.spines['top'].set_visible(False) 
+        pred_frame.spines['bottom'].set_visible(False) 
+        pred_frame.spines['left'].set_visible(False) 
+        pred_frame.spines['right'].set_visible(False) 
+        plt.savefig(exp_name+'/'+filename_no_ext+'_pred_'+str(float(pred))+'.png',\
+            bbox_inches='tight',pad_inches=0,dpi=150)
+
+        plt.close()
+
+        # sio.savemat(exp_name+'/'+filename_no_ext+'_pred_'+str(float(pred))+'.mat',{'data':pred_map})
+
+        diff = den-pred_map
+
+        diff_frame = plt.gca()
+        plt.imshow(diff, 'jet')
+        plt.colorbar()
+        diff_frame.axes.get_yaxis().set_visible(False)
+        diff_frame.axes.get_xaxis().set_visible(False)
+        diff_frame.spines['top'].set_visible(False) 
+        diff_frame.spines['bottom'].set_visible(False) 
+        diff_frame.spines['left'].set_visible(False) 
+        diff_frame.spines['right'].set_visible(False) 
+        plt.savefig(exp_name+'/'+filename_no_ext+'_diff.png',\
+            bbox_inches='tight',pad_inches=0,dpi=150)
+
+        plt.close()
+
+def test2(file_list, model_path):
+
+    net = CrowdCounter(cfg.GPU_ID,cfg.NET)
+    net.load_state_dict(torch.load(model_path))
+    net.cuda()
+    net.eval()
+
+
+    f1 = plt.figure(1)
+
+    gts = []
+    preds = []
+
     difftotal = 0
     difftotalsqr = 0
     MAE = 0
