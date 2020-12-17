94f26c03c746a596a237a7ef24d4a537da04ffce
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2020-12-18 01:51:34 +0700

    s

--- datasets/UCF50/loading_data.py
+++ datasets/UCF50/loading_data.py
@@ -111,6 +111,6 @@ def loading_data():
     train_loader = DataLoader(train_set, batch_size=cfg_data.TRAIN_BATCH_SIZE, num_workers=0, collate_fn=SHHA_collate, shuffle=True, drop_last=True)
     
     val_set = UCF50(cfg_data.DATA_PATH, map(int,str(val_folder)), 'test', main_transform=val_main_transform, img_transform=img_transform, gt_transform=gt_transform)
-    val_loader = DataLoader(val_set, batch_size=cfg_data.VAL_BATCH_SIZE, num_workers=0, shuffle=True, drop_last=False)
+    val_loader = DataLoader(val_set, batch_size=cfg_data.VAL_BATCH_SIZE, num_workers=0, collate_fn=SHHA_collate, shuffle=True, drop_last=False)
 
     return train_loader, val_loader, restore_transform
