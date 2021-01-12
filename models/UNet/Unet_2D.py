f14bae9cd97467f01d822135646f4cda23c2bc8c
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2021-01-13 02:03:05 +0700

    Update Unet_2D.py

--- models/UNet/Unet_2D.py
+++ models/UNet/Unet_2D.py
@@ -88,6 +88,19 @@ class Unet_2D(nn.Module):
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         self.down4 = Down(512, 1024)
+        factor = 2 if bilinear else 1
+        self.down5 = Down(1024, 2048 // factor)
+        self.up1 = Up(2048, 1024 // factor, bilinear)
+        self.up2 = Up(1024, 512 // factor, bilinear)
+        self.up3 = Up(512, 256 // factor, bilinear)
+        self.up4 = Up(256, 128 // factor, bilinear)
+        self.up5 = Up(128, 64, bilinear)
+
+        '''
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
         self.down5 = Down(1024, 2048)
         factor = 2 if bilinear else 1
         self.down6 = Down(2048, 4096 // factor)
@@ -97,9 +110,24 @@ class Unet_2D(nn.Module):
         self.up4 = Up(512, 256 // factor, bilinear)
         self.up5 = Up(256, 128 // factor, bilinear)
         self.up6 = Up(128, 64, bilinear)
+        '''
+
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x6 = self.down5(x5)
+        x = self.up1(x6, x5)
+        x = self.up2(x, x4)
+        x = self.up3(x, x3)
+        x = self.up4(x, x2)
+        x = self.up5(x, x1)
+
+        '''
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
@@ -113,6 +141,8 @@ class Unet_2D(nn.Module):
         x = self.up4(x, x3)
         x = self.up5(x, x2)
         x = self.up6(x, x1)
+        '''
+
         logits = self.outc(x)
         return logits
 
