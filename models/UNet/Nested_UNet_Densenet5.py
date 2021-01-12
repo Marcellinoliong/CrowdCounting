64bbe1899ab462ee6f94cafbd118c039a3e2e577
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2021-01-12 21:40:10 +0700

    s

--- models/UNet/Nested_UNet_Densenet5.py
+++ models/UNet/Nested_UNet_Densenet5.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 from collections import OrderedDict
+from .contextual_layer import ContextualModule
 
 class conv_block_nested(nn.Module):
 
@@ -45,6 +46,104 @@ class conv_block_trans(nn.Module):
         output = self.activation(x)
 
         return output
+
+class _ASPPModule(nn.Module):
+    def __init__(self, inplanes, planes, kernel_size, padding, dilation, BatchNorm):
+        super(_ASPPModule, self).__init__()
+        self.atrous_conv = nn.Conv2d(inplanes, planes, kernel_size=kernel_size,
+                                            stride=1, padding=padding, dilation=dilation, bias=False)
+        self.bn = BatchNorm(planes)
+        self.relu = nn.ReLU()
+
+        self._init_weight()
+
+    def forward(self, x):
+        x = self.atrous_conv(x)
+        x = self.bn(x)
+
+        return self.relu(x)
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, SynchronizedBatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+class ASPP(nn.Module):
+    def __init__(self, backbone, output_stride, BatchNorm):
+        super(ASPP, self).__init__()
+        if backbone == 'drn':
+            inplanes = 512
+        elif backbone == 'mobilenet':
+            inplanes = 320
+        else:
+            inplanes = 2208
+        if output_stride == 16:
+            dilations = [1, 6, 12, 18]
+        elif output_stride == 8:
+            dilations = [1, 12, 24, 36]
+        else:
+            raise NotImplementedError
+
+        self.aspp1 = _ASPPModule(inplanes, 256, 1, padding=0, dilation=dilations[0], BatchNorm=BatchNorm)
+        self.aspp2 = _ASPPModule(inplanes, 256, 3, padding=dilations[1], dilation=dilations[1], BatchNorm=BatchNorm)
+        self.aspp3 = _ASPPModule(inplanes, 256, 3, padding=dilations[2], dilation=dilations[2], BatchNorm=BatchNorm)
+        self.aspp4 = _ASPPModule(inplanes, 256, 3, padding=dilations[3], dilation=dilations[3], BatchNorm=BatchNorm)
+
+        self.global_avg_pool = nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)),
+                                             nn.Conv2d(inplanes, 256, 1, stride=1, bias=False),
+                                             BatchNorm(256),
+                                             nn.ReLU())
+        self.conv1 = nn.Conv2d(256*5, 256, 1, bias=False)
+        self.bn1 = BatchNorm(256)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self._init_weight()
+
+    def forward(self, x):
+        x1 = self.aspp1(x)
+        x2 = self.aspp2(x)
+        x3 = self.aspp3(x)
+        x4 = self.aspp4(x)
+        x5 = self.global_avg_pool(x)
+        x5 = F.interpolate(x5, size=x4.size()[2:], mode='bilinear', align_corners=True)
+        x = torch.cat((x1, x2, x3, x4, x5), dim=1)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        return self.dropout(x)
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, SynchronizedBatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+class ScalePyramidModule(nn.Module):
+    def __init__(self):
+        super(ScalePyramidModule, self).__init__()
+        self.assp = ASPP(2208, output_stride=16, BatchNorm=SynchronizedBatchNorm2d)
+        self.can = ContextualModule(2208, 1024)
+        
+    def forward(self, *input):
+        conv2_2, conv3_3, conv4_3, conv5_3 = input
+        
+        conv4_3 = self.can(conv4_3)
+        conv5_3 = self.assp(conv5_3)
+        
+        return conv2_2, conv3_3, conv4_3, conv5_3
 
 class Nested_UNet_Densenet5(nn.Module):
 
@@ -91,6 +190,8 @@ class Nested_UNet_Densenet5(nn.Module):
 
         self.dense = models.densenet161(pretrained=True) 
 
+        self.spm = ScalePyramidModule()
+
         #self.trans = nn.Conv2d(in_channels=2208, out_channels=64, kernel_size=1, bias=False)
         #self.trans0 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=1, bias=False)
         #self.trans1 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=1, bias=False)
@@ -110,8 +211,13 @@ class Nested_UNet_Densenet5(nn.Module):
         x_dn = self.dense.features(x)
         #print(x_dn.size())
 
-        x5_0 = self.trans5(x_dn)
-        x4_0 = self.trans4(x5_0)
+        x_dn = self.spm(*x_dn)
+        print(x_dn.size())
+
+        #x5_0 = self.trans5(x_dn)
+        #x5_0 = x_dn
+        #x4_0 = self.trans4(x5_0)
+        x4_0 = x_dn
         x3_0 = self.trans3(x4_0)
         x2_0 = self.trans2(x3_0)
         x1_0 = self.trans1(x2_0)
