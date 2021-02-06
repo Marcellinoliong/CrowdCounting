737fdb7dcb2136eb9ea48e4bd737c46fae56aa7b
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2021-02-07 02:00:43 +0700

    Update Nested_UNet_Densenet5.py

--- models/UNet/Nested_UNet_Densenet5.py
+++ models/UNet/Nested_UNet_Densenet5.py
@@ -104,6 +104,46 @@ class ScalePyramidModule(nn.Module):
         
         return conv2_2, conv3_3, conv4_3, conv5_3
 
+class conv_block_nested(nn.Module):
+    def __init__(self, in_ch, mid_ch, out_ch):
+        super(conv_block_nested, self).__init__()
+        self.activation = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=3, padding=1, stride=1, bias=True)
+        self.bn1 = nn.BatchNorm2d(mid_ch)
+        self.conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=3, padding=1, stride=1, bias=True)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        output = self.activation(x)
+
+        return output
+
+class conv_block_trans(nn.Module):
+    def __init__(self, in_ch, mid_ch, out_ch):
+        super(conv_block_trans, self).__init__()
+        self.activation = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=2, padding=1, stride=1, bias=True)
+        self.bn1 = nn.BatchNorm2d(mid_ch)
+        self.conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=2, padding=1, stride=1, bias=True)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        output = self.activation(x)
+
+        return output
+        
 class Nested_UNet_Densenet5(nn.Module):
 
     def __init__(self, in_ch=3, out_ch=1,  pretrained=True):
