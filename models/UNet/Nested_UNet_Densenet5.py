6ece68aece76c17d56a3f3fd02bde128e0dbc259
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2021-02-08 23:39:55 +0700

    Update Nested_UNet_Densenet5.py

--- models/UNet/Nested_UNet_Densenet5.py
+++ models/UNet/Nested_UNet_Densenet5.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 from collections import OrderedDict
-from contextual_layer import ContextualModule
+from .contextual_layer import ContextualModule
 from torch.utils import model_zoo
 
 class conv_block_nested(nn.Module):
