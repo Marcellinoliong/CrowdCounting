caaf7ee5389c871f9413dbb8b7e398c6ba84ff23
Marcellinoliong <35723130+Marcellinoliong@users.noreply.github.com>
2021-02-16 01:24:28 +0700

    Update CC.py

--- models/CC.py
+++ models/CC.py
@@ -59,13 +59,13 @@ class CrowdCounter(nn.Module):
 
         
 
-        #if len(gpus)>1:
-        #    self.CCN = torch.nn.DataParallel(self.CCN, device_ids=gpus).cuda()
-        #else:
-        #    self.CCN=self.CCN.cuda()
-        #self.loss_mse_fn = nn.MSELoss().cuda()
-        self.CCN=self.CCN.cpu()
-        self.loss_mse_fn = nn.MSELoss().cpu()
+        if len(gpus)>1:
+            self.CCN = torch.nn.DataParallel(self.CCN, device_ids=gpus).cuda()
+        else:
+            self.CCN=self.CCN.cuda()
+        self.loss_mse_fn = nn.MSELoss().cuda()
+        #self.CCN=self.CCN.cpu()
+        #self.loss_mse_fn = nn.MSELoss().cpu()
         
     @property
     def loss(self):
